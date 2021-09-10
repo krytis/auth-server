e9426b7d54c7712d73bd7cc5eb90233933b269a2
Annika <annika0uwu@gmail.com>
2021-09-09 22:04:08 -0700

    Use process.env for configuration

--- src/config-loader.ts
+++ src/config-loader.ts
@@ -2,6 +2,12 @@
  * Loads configuration
  */
 
+import * as path from 'path';
+import * as fs from 'fs';
+
+const BAD_PORT_ERROR = (
+    `PORT must be specified in .env or as an environment variable as an integer between 1 and 49151.`
+);
 export interface Config {
     /** The port that the server should listen on */
     port: number;
@@ -10,16 +16,25 @@ export interface Config {
 }
 
 function loadConfig(): Config {
-    try {
-        /* eslint-disable-next-line @typescript-eslint/no-var-requires */
-        return require('./config').configuration;
-    } catch (err: any) {
-        if (err.code === 'MODULE_NOT_FOUND') {
-            throw new Error(`Cannot load configuration file; check that src/config.ts exists.`);
-        } else {
-            throw err;
-        }
+    if (!process.env.PORT) throw new Error(BAD_PORT_ERROR);
+    const port = parseInt(process.env.PORT);
+    if (port < 1 || port > 49151) throw new Error(BAD_PORT_ERROR);
+    if (Math.floor(port) !== port) throw new Error(BAD_PORT_ERROR);
+
+    const root = path.resolve(__dirname, '..');
+    if (!process.env.DATABASE_PATH) {
+        throw new Error(
+            `DATABASE_PATH must be specified in .env or as an environment variable ` +
+            `as a path (absolute or relative to ${root}) to a SQLite database.`
+        );
     }
+    const databasePath = path.resolve(root, process.env.DATABASE_PATH);
+    const directory = path.parse(databasePath).dir;
+    if (!fs.existsSync(directory) || !fs.statSync(directory).isDirectory()) {
+        throw new Error(`The database specified in DATABASE_PATH must be in a directory that exists.`);
+    }
+
+    return {port, databasePath};
 }
 
 export const config = loadConfig();
