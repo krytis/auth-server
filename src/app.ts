3271350fba6c4468734c78d90367d718f689d534
Annika <annika0uwu@gmail.com>
2021-09-13 22:55:08 -0700

    Remove completed TODO

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,6 @@
 import {config} from './config';
 import {startServer} from './routes';
 
-// TODO:
-// - tests for routes (start server? use Fastify?)s
-
 startServer(config.port, config.listenAddress).then(
     () => console.log(`Authentication server listening on http://${config.listenAddress}:${config.port}/`),
     err => console.error(`Error starting server: ${err as string}`),
